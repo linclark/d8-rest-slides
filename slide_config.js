6be7a706df15ef46ad7e54f2a30d2a63e8056aa4
Lin Clark <lin.w.clark@gmail.com>
2013-06-29 15:22:52 +0100

    First pages commit

---
+++ slide_config.js
@@ -0,0 +1,34 @@
+var SLIDE_CONFIG = {
+  // Slide settings
+  settings: {
+    title: 'REST and Serialization<br>in Drupal 8',
+    subtitle: 'Connecting Drupal and Web Services',
+    eventTitle: 'DrupalDevDays Dublin 2013',
+    useBuilds: true, // Default: true. False will turn off slide animation builds.
+    usePrettify: true, // Default: true
+    enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
+    enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
+    //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
+    favIcon: 'images/devdays-dublin-2013.png',
+    fonts: [
+      'Open Sans:regular,semibold,italic,italicsemibold',
+      'Source Code Pro'
+    ],
+    //theme: ['mytheme'], // Add your own custom themes or styles in /theme/css. Leave off the .css extension.
+  },
+
+  // Author information
+  presenters: [{
+    name: 'Lin Clark',
+    company: 'Independent developer',
+    twitter: '@linclark',
+    www: 'http://lin-clark.com',
+    github: 'http://github.com/linclark'
+  }, {
+    name: 'Klaus Purer',
+    company: 'Developer, epiqo.com',
+    twitter: '@_klausi_',
+    www: 'http://klau.si',
+  }]
+};
+
