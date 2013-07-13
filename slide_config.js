1626922eb8882f2a162c3277fbc171cefb578507
Lin Clark <lin.w.clark@gmail.com>
2013-07-13 08:41:55 -0400

    Changed image to NYC Camp logo.

--- slide_config.js
+++ slide_config.js
@@ -3,13 +3,13 @@ var SLIDE_CONFIG = {
   settings: {
     title: 'REST and Serialization<br>in Drupal 8',
     subtitle: 'Connecting Drupal and Web Services',
-    eventTitle: 'DrupalDevDays Dublin 2013',
+    eventTitle: 'NYC Camp 2013',
     useBuilds: true, // Default: true. False will turn off slide animation builds.
     usePrettify: true, // Default: true
     enableSlideAreas: true, // Default: true. False turns off the click areas on either slide of the slides.
     enableTouch: true, // Default: true. If touch support should enabled. Note: the device must support touch.
     //analytics: 'UA-XXXXXXXX-1', // TODO: Using this breaks GA for some reason (probably requirejs). Update your tracking code in template.html instead.
-    favIcon: 'images/devdays-dublin-2013.png',
+    favIcon: 'images/logo-drupal-camp-nyc-eyes.png',
     fonts: [
       'Open Sans:regular,semibold,italic,italicsemibold',
       'Source Code Pro'
